1865e5d617af4adc577f7c1e61351f801730462c
Shun Usami <usatie@gmail.com>
2023-02-06 21:08:08 +0900

    Add all.h

--- ezoe_cpp/hello.cpp
+++ ezoe_cpp/hello.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 // g++ [options] -o output_filename input_filenames
-// g++ -o hello hello.c 
+// g++ -o hello hello.cpp
+// g++ -std=c++17 -Wall --pedantic-errors -o hello hello.cpp
+// man gcc
+// g++ -include all.h -o program main.cpp
 int	main()
 {
 	std::cout << "hello" ;
