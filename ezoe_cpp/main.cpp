2bc949495ecdbc1875bc918c4b8b105050832743
Shun Usami <usatie@gmail.com>
2023-02-06 22:09:50 +0900

    Variables

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -2,37 +2,78 @@
 
 int	main()
 {
-	std::cout
-		<< "Integer: "s << 42 << "\n"s
-		<< "Floating Point: "s << 3.14 ;
+	auto answer = 42 ;
+	std::cout << answer << "\n"s ;
+
+	auto pi = 3.14 ;
+	std::cout << pi << "\n"s ;
+
+	auto question = "Life, The Universe, and Everything."s ;
+	std::cout << question ;
+
+	auto a = 1 ;
+	auto b(2) ;
+	auto c{3} ;
+
+	/* compile error
+	std::cout << x ;
+	auto x = 123 ;
+	*/
+
+	auto x
+	= 123 ;
+
+	std::cout << x;
+
+	x = 456 ;
+
+	std::cout << x;
+
+	x = 789 ;
+
+	std::cout << x;
+	std::cout << "\n"s ;
+
+	// self assignment
+	x = 10 ;
+	x = x + 5 ;
+
+	std::cout << x;
+	std::cout << "\n"s ;
 	
+	// Add 
+	x = 1 ;
+	x += 5 ;
+
+	std::cout << x;
 	std::cout << "\n"s ;
 
-	std::cout
-		<< 3 + 5 << " "s << 3 - 5 << " "s
-		<< 3 * 5 << " "s << 3 / 5 << " "s
-		<< 3 % 5 ;
+	// Statically typed
+	/* compile error
+	auto x = 1 ;
+	x = "hello"s ;
+	x = 2 ;
+	*/
+
+	auto i = 123 ; // int
+	auto d = 1.23 ; // double
+	auto s = "123"s ; // std::string
 	
-	std::cout << "\n"s ;
 
-	std::cout << (1 + 2) * 3;
+	/* ok
+	int a = 3.14 ;
+	std::cout << a << "\n"s ;
 
-	std::cout << "\n"s ;
-
-	// mod is invalid in floats
-	std::cout
-		<< 3.5 + 7.11 << " "s << 3.5 - 7.11 << " "s
-		<< 3.5 * 7.11 << " "s << 3.5 / 7.11 ;
-
-	std::cout << "\n"s ;
-
-	std::cout << 1 + 0.1 ;
+	double d = 123 ;
+	std::cout << d;
 	
-	std::cout << "\n"s ;
+	int i = 0.9999 ;
+	std::cout << i ; // 0
 
-	// Compile error
-	// std::cout << 1 + "234"s ;
-
-	// Maybe in this case, "234" is a pointer
-	std::cout << 1 + "234" ;
+	double d = 1234567890 ;
+	std::cout << d ;
+	*/
+	
+	d = 1234567890 ;
+	std::cout << d ;
 }
