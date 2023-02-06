9162d9d13c3936ba96c8e9880aada2d87f844796
Shun Usami <usatie@gmail.com>
2023-02-06 22:24:17 +0900

    Lambda Function

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -1,79 +1,54 @@
-#include "all.h"
-
 int	main()
 {
-	auto answer = 42 ;
-	std::cout << answer << "\n"s ;
+	// lambda statement
+	auto print = [](auto x)
+	{
+		std::cout << x << "\n"s ;
+	} ;
+	print(123) ;
+	print(3.14) ;
+	print("hello") ;
 
-	auto pi = 3.14 ;
-	std::cout << pi << "\n"s ;
+	auto print_two = []( auto x, auto y )
+	{
+		std::cout << x << " "s << y << "\n"s ;
+	} ;
 
-	auto question = "Life, The Universe, and Everything."s ;
-	std::cout << question ;
+	print_two( 1, 2 ) ;
+	print_two( "Pi is", 3.14 ) ;
 
-	auto a = 1 ;
-	auto b(2) ;
-	auto c{3} ;
+	auto no_args = []()
+	{
+		std::cout << "Nothing\n" ;
+	} ;
+	no_args() ;
 
-	/* compile error
-	std::cout << x ;
-	auto x = 123 ;
-	*/
+	// () is operator
+	// operator ()
+	auto donothing = [](){} ;
+	donothing() ;
+	donothing     (     ) ;
 
-	auto x
-	= 123 ;
-
-	std::cout << x;
-
-	x = 456 ;
-
-	std::cout << x;
-
-	x = 789 ;
-
-	std::cout << x;
-	std::cout << "\n"s ;
-
-	// self assignment
-	x = 10 ;
-	x = x + 5 ;
-
-	std::cout << x;
-	std::cout << "\n"s ;
+	// operato() on lambda itself
+	[](){}() ;
 	
-	// Add 
-	x = 1 ;
-	x += 5 ;
+	// Return value
+	auto plus = []( auto x, auto y )
+	{
+		return x + y ;
+	} ;
+	std::cout
+		<< plus( 1, 2 ) << "\n"s
+		<< plus( 1.5, 0.5 ) << "\n"s
+		<< plus( "123"s, "456"s ) << "\n"s ;
 
-	std::cout << x;
-	std::cout << "\n"s ;
+	// Exit on return
+	auto f = []()
+	{
+		std::cout << "f is called.\n"s ;
+		return 0 ;
+		std::cout << "f returned zero.\n" ;
+	} ;
 
-	// Statically typed
-	/* compile error
-	auto x = 1 ;
-	x = "hello"s ;
-	x = 2 ;
-	*/
-
-	auto i = 123 ; // int
-	auto d = 1.23 ; // double
-	auto s = "123"s ; // std::string
-	
-
-	/* ok
-	int a = 3.14 ;
-	std::cout << a << "\n"s ;
-
-	double d = 123 ;
-	std::cout << d;
-	
-	int i = 0.9999 ;
-	std::cout << i ; // 0
-
-	double d = 1234567890 ;
-	std::cout << d ;
-	*/
-	
-	d = 1234567890 ;
-	std::cout << d ;
+	auto result = f() ;
 }
