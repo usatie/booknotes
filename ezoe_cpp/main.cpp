3bd6559b3e91b74fc5cb66ef80eabce490665e99
Shun Usami <usatie@gmail.com>
2023-02-06 21:49:27 +0900

    Integers and Floating-point numbers

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -2,10 +2,37 @@
 
 int	main()
 {
-	// What's the difference of "" and ""s ?
-	std::cout << "hello"s ;
-	std::cout << "hello" ;
-	std::cout << "aaa\nbbb\nccc"s ;
-	std::cout << "\\n is a new-line.\n"s ;
-	std::cout << "hello"s + "world"s ;
+	std::cout
+		<< "Integer: "s << 42 << "\n"s
+		<< "Floating Point: "s << 3.14 ;
+	
+	std::cout << "\n"s ;
+
+	std::cout
+		<< 3 + 5 << " "s << 3 - 5 << " "s
+		<< 3 * 5 << " "s << 3 / 5 << " "s
+		<< 3 % 5 ;
+	
+	std::cout << "\n"s ;
+
+	std::cout << (1 + 2) * 3;
+
+	std::cout << "\n"s ;
+
+	// mod is invalid in floats
+	std::cout
+		<< 3.5 + 7.11 << " "s << 3.5 - 7.11 << " "s
+		<< 3.5 * 7.11 << " "s << 3.5 / 7.11 ;
+
+	std::cout << "\n"s ;
+
+	std::cout << 1 + 0.1 ;
+	
+	std::cout << "\n"s ;
+
+	// Compile error
+	// std::cout << 1 + "234"s ;
+
+	// Maybe in this case, "234" is a pointer
+	std::cout << 1 + "234" ;
 }
