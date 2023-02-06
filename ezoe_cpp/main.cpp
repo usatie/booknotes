540f96a371b68a8c2e96cb9f381758f2184bf838
Shun Usami <usatie@gmail.com>
2023-02-07 08:15:39 +0900

    Debugging

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -1,49 +1,16 @@
-// return_type func_name (args) {body}
-
-int plus ( int x, int y )
-{
-	return x + y ;
-}
-
-double plus ( double x, double y )
-{
-	return x + y ;
-}
-
-std::string plus ( std::string x, std::string y )
-{
-	return x + y ;
-}
-
-
-/* Compile error
-int f()	{}
-*/
-
-// ok
-void f(){}
-
-auto a() {}
-auto b() { return 0 ; }
-auto c() { return 0.0 ; }
-auto d() { return ""s ; }
-
-/* compile error type 
-auto e()
-{
-	return 0 ;
-	return 0.0 ;
-}
-*/
-
 int	main()
 {
-	auto x = plus( 1, 2 ) ;
-	std::cout << x << "\n" ;
+	// compile error
+	// auto x = 1 + 1 
 
-	auto d = plus( 1.1, 2.04 ) ;
-	std::cout << d << "\n" ;
+	auto x = 1 + 1 ;
+	// unused warning
+	auto y = x + 1 ;
 
-	auto s = plus( "hello"s, "world"s ) ;
-	std::cout << s << "\n" ;
+	std::cout
+		<< "EXIT_SUCCESS: "s << EXIT_SUCCESS << "\n"s
+		<< "EXIT_FAILURE: "s << EXIT_FAILURE << "\n"s ;
+
+	// compile error
+	// "hello"s << 1 ;
 }
