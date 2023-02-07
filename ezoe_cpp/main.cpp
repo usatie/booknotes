213af0c6e99fe4318ac0a70dab51a07b7a1010db
Shun Usami <usatie@gmail.com>
2023-02-07 18:01:18 +0900

    goto loop

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -1,8 +1,56 @@
+void	hello()
+{
+	std::cout << "hello\n"s ;
+}
+
+int	input()
+{
+	std::cout << ">"s ;
+	int	x {} ;
+	std::cin >> x ;
+	return x ;
+}
+
+void	hello_n( int n )
+{
+	int	i = 0;
+	if ( n < 0 )
+		return ;
+loop:
+	if ( i != n )
+	{
+		std::cout << "hello\n"s ;
+		++i ;
+		goto loop ;
+	}
+}
+
 int	main()
 {
-	double height{ } ;
-	double mass{ } ;
-	
-	std::cin >> height >> mass ;
-	std::cout << mass / (height * height) ;
+	/*
+	std::cout << 1 ;
+	goto skip ;
+	std::cout << 2;
+skip:
+	std::cout << 3 ;
+	*/
+
+	/*
+loop:
+	hello() ;
+	goto loop ;
+	*/
+
+	/*
+	int sum = 0 ;
+loop:
+	int x = input() ;
+	if ( x != 0 )
+	{
+		sum += x ;
+		std::cout << sum << "\n" ;
+		goto loop ;
+	}
+	*/
+	hello_n(10);
 }
