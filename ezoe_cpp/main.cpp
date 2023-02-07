71de1029f0f70878cf7df1d0578a6d558cc0e963
Shun Usami <usatie@gmail.com>
2023-02-07 09:11:17 +0900

    If statement

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -1,41 +1,97 @@
 int	main()
 {
-	/*
-	// statements
-	auto x = 1 + 1 ;
-	std::cout << x ;
-	;
-	*/
+	auto a = 12345 + 6789 ;
+	auto b = 8073 * 132 / 5 ;
+	auto sum = a + b ;
 
-	// compound statements
+	std::cout
+		<< "a=12345 + 6789="s << a << "\n"s
+		<< "b=8073 * 132 / 5="s << b << "\n"s
+		<< "a+b="s << sum << "\n"s ;
+	if ( a < b )
 	{
-		std::cout << "hello\n"s ;
-		std::cout << "hello\n"s ;
+		std::cout << b << "\n"s ;
 	}
-	{ std::cout << "world\n"s ; }
-	{ }
-	{{{}}}
-
-	// variable scope
-	auto a = 0 ;
+	else
 	{
-		auto b = 0 ;
+		std::cout << a << "\n"s ;
+	}
+
+	if ( 2 < 1 )
+		std::cout << "sentence 1.\n"s ;
+	std::cout << "sentence 2.\n"s ;
+
+	if ( 1 < 2 )
+	{
+		std::cout << "yes!\n"s ;
+		std::cout << "yes!\n"s ;
+	}
+
+	{
+		auto a = "dog"s ;
+		auto b = "dog"s ;
+		auto c = "cat"s ;
+		
+		if ( a == b )
 		{
-			auto c = 0 ;
+			std::cout << "a == b\n"s ;
+		}
+		else
+		{
+			std::cout << "a != b\n" ;
+		}
+
+		if ( a == c )
+		{
+			std::cout << "a == c\n"s ;
+		}
+		else
+		{
+			std::cout << "a != c\n" ;
 		}
 	}
 
-	// shadow
-	auto x = 0 ;
 	{
-		auto x = 1 ;
+		auto dog = "dog"s ;
+		auto cat = "cat"s ;
+		
+		if ( cat < dog )
 		{
-			auto x = 2 ;
-			std::cout << x << "\n"s ;
+			std::cout << "cat is smaller.\n"s ;
 		}
-		std::cout << x << "\n"s ;
-		x = 42 ;
-		std::cout << x << "\n"s ;
+		else
+		{
+			std::cout << "dog is smaller.\n" ;
+		}
+
+		auto longcat = "longcat"s ;
+		if ( longcat > cat )
+		{
+			std::cout << "Longcat is Looong.\n"s ;
+		}
+		else
+		{
+			std::cout << "Longcat isn't that long. Sigh.\n" ;
+		}
 	}
-	std::cout << x << "\n"s ;
+	{
+		auto x = ""s ;
+		
+		if ( "a"s < "b"s )
+		{ x = "a"s ; }
+		else
+		{ x = "b"s ; }
+
+		std::cout << x << "\n" ;
+	}
+	{
+		auto x = ""s ;
+		
+		if ( "aa"s < "ab"s )
+		{ x = "aa"s ; }
+		else
+		{ x = "ab"s ; }
+
+		std::cout << x << "\n" ;
+	}
 }
