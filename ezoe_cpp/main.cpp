e7f8273db84207f80af7ddaa25347f9f04b165e7
Shun Usami <usatie@gmail.com>
2023-02-07 20:36:20 +0900

    for loop

--- ezoe_cpp/main.cpp
+++ ezoe_cpp/main.cpp
@@ -1,76 +1,25 @@
-int	input()
+// for ( init ; cond ; increment ) statement
+void	print_n( int n )
 {
-	std::cout << ">"s ;
-	int	x {} ;
-	std::cin >> x ;
-	return x ;
-}
-
-void	sum_input_loop()
-{
-	int sum = 0 ;
-	int x {} ;
-
-	while ( ( x = input() ) != 0 )
-	{
-		sum += x ;
-		std::cout << sum << "\n"s ;
-	}
-}
-
-void	print_times_table_while()
-{
-	int a = 1 ;
-	while ( a <= 9 )
-	{
-		int b = 1 ;
-		while ( b <= 9 )
-		{
-			std::cout << a * b << "\t"s ;
-			++b ;
-		}
-		std::cout << "\n"s ;
-		++a ;
-	}
-}
-
-void	print_times_table_goto_ezoe()
-{
-	int a = 1 ;
-loop_outer:
-	if ( a <= 9 )
-	{
-		int b = 1 ;
-loop_inner:
-		if ( b <= 9 )
-		{
-			std::cout << a * b << "\t"s ;
-			++b ;
-			goto loop_inner ;
-		}
-		std::cout << "\n"s ;
-		++a ;
-		goto loop_outer ;
-	}
-}
-
-void	print_times_table_goto()
-{
-	int a = 1 ;
-loop1:
-	int b = 1 ;
-loop2:
-	std::cout << a * b << "\t"s ;
-	++b ;
-	if ( b <= 9 )
-		goto loop2 ;
+	for ( int i = 0 ; i < n ; ++i )
+		std::cout << i << " "s ;
 	std::cout << "\n"s ;
-	++a ;
-	if ( a <= 9 )
-		goto loop1 ;
+}
+
+void	print_times_table_for()
+{
+	for ( int a = 1 ; a <= 9 ; ++a )
+	{
+		for ( int b = 1 ; b <= 9 ; ++b )
+		{
+			std::cout << a * b << "\t"s ;
+		}
+		std::cout << "\n"s ;
+	}
 }
 
 int	main()
 {
-	print_times_table_goto_ezoe() ;
+	print_n( 100 ) ;
+	print_times_table_for() ;
 }
