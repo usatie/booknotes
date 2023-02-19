3499cc4afecf9adb3d6e978243ad3d8766ba5b08
Shun Usami <usatie@gmail.com>
2023-02-19 09:48:14 +0900

    Playground cin.ignore()

--- ezoe_cpp/99_playground/main.cpp
+++ ezoe_cpp/99_playground/main.cpp
@@ -1,6 +1,11 @@
+#include <string>
+
 int	main()
 {
-	std::string s = "hello"s ;
-	for ( auto i = s.begin() ; i != s.end() ; ++i )
-		std::cout << *i << "\n"s ;
+	std::string s ;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), 'C') ;
+	while (std::getline(std::cin, s) )
+	{
+		std::cout << "[" << s << "]\n" ;
+	}
 }
