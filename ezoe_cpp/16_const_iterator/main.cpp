1917fc9287eb8f9191d1e6888e2dfe65d4309f48
Shun Usami <usatie@gmail.com>
2023-02-19 18:20:03 +0900

    array_const_iterator

--- ezoe_cpp/16_const_iterator/main.cpp
+++ ezoe_cpp/16_const_iterator/main.cpp
@@ -1,2 +1,39 @@
+template < typename Array >
+struct array_iterator
+{
+	Array & a ;
+	std::size_t i ;
+
+	array_iterator( Array & a, std::size_t i )
+		: a(a), i(i) { }
+} ;
+
+template < typename Array >
+struct array_const_iterator
+{
+	Array const & a ;
+	std::size_t i ;
+
+	array_const_iterator( Array const & a, std::size_t i )
+		: a(a), i(i) { }
+	array_const_iterator( array_iterator<Array> const & iter )
+		: a(iter.a), i(iter.i) { }
+} ;
+
+template < typename T, std::size_t N >
+struct array
+{
+	using iterator = array_iterator<array> ;
+	using const_iterator = array_const_iterator<array> ;
+
+	// Return const_iterator if array is const
+	const_iterator begin() const { return const_iterator(*this, 0) ; }
+	const_iterator end() const { return const_iterator(*this, N) ; }
+	// Always return const_iterator
+	const_iterator cbegin() const { return const_iterator(*this, 0) ; }
+	const_iterator cend() const { return const_iterator(*this, N) ; }
+} ;
+
 int main()
-{}
+{
+}
