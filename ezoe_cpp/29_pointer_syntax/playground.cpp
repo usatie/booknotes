f6533832ce508e5f62f1f8e18583b47082c0be58
Shun Usami <usatie@gmail.com>
2023-10-12 11:31:21 +0900

    Ezoe c++ 29

--- ezoe_cpp/29_pointer_syntax/playground.cpp
+++ ezoe_cpp/29_pointer_syntax/playground.cpp
@@ -5,6 +5,12 @@
 // const T : constant T
 // T const : constant T
 
+int f( int x )
+{
+	std::cout << x << "\n" ;
+	return x ;
+}
+
 int	main()
 {
 	// T *     : ponter to T
@@ -85,5 +91,95 @@ int	main()
 		//*ptr = 42       // NOT OK
 		data3 = 42 ;      // ok
 	}
-	// 
+	// pointer to pointer
+	{
+		using U = int ;
+		using T = U * ; // pointer to T
+		using type = T * ; // pointer to pointer
+	}
+	{
+		int x = 123 ;
+		int * p = &x ;
+		int ** pp = &p ;
+
+		int value1 = **pp ; // get value x from pp
+		std::cout << "value1: " << value1 << "\n" ;
+		int y = 456 ;
+		*pp = &y ; // update value p
+		int value2 = **pp ; // get calue y from pp
+		std::cout << "value2: " << value2 << "\n" ;
+	}
+	{
+		int object { } ;
+
+		int * a = &object ;
+		int ** b = &a ;
+		int ** pointer_to_pointer_to_object = b ;
+
+		int *c = *pointer_to_pointer_to_object ;
+
+		std::cout << "object: " << object << "\n" ;
+		*c = 1 ; // object = 1
+		std::cout << "object: " << object << "\n" ;
+		**b = 2 ; // object = 2
+		std::cout << "object: " << object << "\n" ;
+	}
+	{
+		int object { } ;
+
+		int * a = &object ;
+		int ** b = &a ;
+
+		int & r1 = *a ;
+		int & r2 = **b ;
+
+		std::cout << "object: " << object << "\n" ;
+		r1 = 1 ;
+		std::cout << "object: " << object << "\n" ;
+		r2 = 2 ;
+		std::cout << "object: " << object << "\n" ;
+	}
+	{
+		using type = int *** ;
+		using pointer_to_type = type * ;
+	}
+	{
+		using type = int const * const * const * const ;
+	}
+	// pointer to function
+	{
+		int f( int ) ;
+		double g( double, double ) ;
+
+		using f_type = int ( int ) ;
+		using g_type = double ( double, double ) ;
+
+		using f_pointer = f_type * ;
+		using g_pointer = g_type * ;
+	}
+	// call function via pointer
+	{
+		using f_type = int ( int ) ;
+		using f_pointer = f_type * ;
+		/*
+		 * Lambda cannot be used as function pointer?
+		auto f = []( int x ) -> int {
+			std::cout << x << "\n" ;
+			return x ;
+		}
+		*/
+		f_pointer ptr = &f ;
+		(*ptr)(123) ;
+		ptr(456) ; // pointer to function can be automatically dereferenced
+	}
+	// declare pointer to function without type alias
+	{
+		int (*ptr)(int) = &f ;
+		int * name1 ;
+		int (*name2	) ;
+		using type1 = int * ;
+		using type2 = int (* /* name is omitted */ ) ;
+		int * f2( int ); // function returns pointer to int
+		using type3 = int (* /* name is omitted */ )(int) ; // function pointer
+	}
 }
