44284a22129c0cffbdd6af709c4d246a359c0da8
Shun Usami <usatie@gmail.com>
2023-10-19 21:50:10 +0900

    Riakuto 6-3. Vite

--- reakuto/ch6/vite-app/src/App.tsx
+++ reakuto/ch6/vite-app/src/App.tsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const title = import.meta.env.VITE_APP_TITLE
+console.dir(import.meta.env)
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{title}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
