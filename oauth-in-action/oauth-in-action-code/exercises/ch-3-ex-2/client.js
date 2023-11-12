f133be01c5a0254c50de0d4787367ff267f865a7
Shun Usami <usatie@gmail.com>
2023-11-12 17:16:47 +0900

    [oauth] Implement exercise 3-2

--- oauth-in-action/oauth-in-action-code/exercises/ch-3-ex-2/client.js
+++ oauth-in-action/oauth-in-action-code/exercises/ch-3-ex-2/client.js
@@ -136,11 +136,15 @@ app.get('/fetch_resource', function(req, res) {
 		/*
 		 * Instead of always returning an error like we do here, refresh the access token if we have a refresh token
 		 */
-		console.log("resource status error code " + resource.statusCode);
-		res.render('error', {error: 'Unable to fetch resource. Status ' + resource.statusCode});
+		access_token = null;
+		if (refresh_token) {
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', {error: 'Unable to fetch resource. Status ' + resource.statusCode});
+			return;
+		}
 	}
-	
-	
 });
 
 var refreshAccessToken = function(req, res) {
@@ -149,6 +153,43 @@ var refreshAccessToken = function(req, res) {
 	 * Use the refresh token to get a new access token
 	 */
 	
+	var form_data = qs.stringify({
+		grant_type: 'refresh_token',
+		refresh_token: refresh_token,
+	});
+
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret),
+	};
+
+	console.log('Refreshing token %s', refresh_token);
+
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		var body = JSON.parse(tokRes.getBody());
+		access_token = body.access_token;
+
+		if (body.refresh_token) {
+			refresh_token = body.refresh_token;
+			console.log('Got refresh token: %s', refresh_token);
+		}
+
+		scope = body.scope;
+		console.log('Got scope: %s', scope);
+
+		res.redirect('/fetch_resource');
+		return;
+	} else {
+		console.log('No refresh token, asking the user to get a new access token');
+		refresh_token = null;
+		res.render('error', {error: 'Unable to refresh token.'});
+		return;
+	}
 };
 
 var buildUrl = function(base, options, hash) {
