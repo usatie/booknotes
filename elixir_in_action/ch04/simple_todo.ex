ab31aef5d33d7122841fa6b9d907ba87845c3b95
Shun Usami <usatie@gmail.com>
2024-05-22 23:31:44 -0700

    [Elixir] 4.1.3 Structuring data with maps

--- elixir_in_action/ch04/simple_todo.ex
+++ elixir_in_action/ch04/simple_todo.ex
@@ -1,8 +1,8 @@
 defmodule TodoList do
   def new(), do: MultiDict.new()
 
-  def add_entry(todo_list, date, title) do
-    MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
   def entries(todo_list, date) do
