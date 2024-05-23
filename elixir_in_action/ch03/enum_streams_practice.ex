383b79a32bc11f153dac8995d1c54495e5e0a71f
Shun Usami <usatie@gmail.com>
2024-05-22 21:31:46 -0700

    [Elixir] Refactor enum_streams_practice by Sasa's code

--- elixir_in_action/ch03/enum_streams_practice.ex
+++ elixir_in_action/ch03/enum_streams_practice.ex
@@ -1,35 +1,44 @@
 defmodule EnumStreams do
-  def large_lines!(path) do
-    File.stream!(path)
+  defp filtered_lines!(path) do
+    path
+    |> File.stream!()
     |> Stream.map(&String.trim_trailing(&1, "\n"))
+  end
+
+  def large_lines!(path) do
+    path
+    |> filtered_lines!
     |> Enum.filter(&(String.length(&1) > 80))
   end
 
   def lines_lengths!(path) do
-    File.stream!(path)
-    |> Stream.map(&String.trim_trailing(&1, "\n"))
-    |> Enum.map(&(String.length(&1)))
+    path
+    |> filtered_lines!
+    |> Enum.map(&String.length/1)
   end
 
   def longest_line_length!(path) do
-    File.stream!(path)
-    |> Stream.map(&String.trim_trailing(&1, "\n"))
-    |> Stream.map(&(String.length(&1)))
-    |> Enum.reduce(0, &(max(&1, &2)))
+    path
+    |> filtered_lines!
+    |> Stream.map(&String.length/1)
+    |> Enum.max()
   end
 
   def longest_line!(path) do
-    File.stream!(path)
-    |> Stream.map(&String.trim_trailing(&1, "\n"))
-    |> Stream.map(&(%{line: &1, len: String.length(&1)}))
-    |> Enum.reduce(%{line: "", len: 0}, &(if &1[:len] >= &2[:len], do: &1, else: &2))
-    |> Map.get(:line)
+    path
+    |> filtered_lines!
+    |> Enum.max_by(&String.length/1)
   end
 
   def words_per_line!(path) do
-    File.stream!(path)
-    |> Stream.map(&String.trim_trailing(&1, "\n"))
-    |> Stream.map(&String.split(&1))
-    |> Enum.map(&Enum.count(&1))
+    path
+    |> filtered_lines!
+    |> Enum.map(&word_count/1)
+  end
+
+  defp word_count(string) do
+    string
+    |> String.split()
+    |> length()
   end
 end
