42d8bfdae548af108e07f4fdbf9a6da5b7f3ba99
Shun Usami <usatie@gmail.com>
2024-05-23 16:44:19 -0700

    [Elixir] Refactor improts

--- elixir_in_action/ch04/todo_import.ex
+++ elixir_in_action/ch04/todo_import.ex
@@ -1,19 +1,25 @@
 defmodule TodoList.CsvImporter do
-  defp filtered_lines!(path) do
+  def import(path) do
+    path
+    |> read_lines
+    |> create_entries
+    |> TodoList.new()
+  end
+
+  defp read_lines(path) do
     path
     |> File.stream!()
     |> Stream.map(&String.trim_trailing(&1, "\n"))
   end
 
-  defp to_date!(date_string) do
-    Date.from_iso8601!(date_string)
-  end
-
-  def import(path) do
-    path
-    |> filtered_lines!                                                        # line
-    |> Stream.map(&(String.split(&1, ",")))                                   # [date_string, title]
-    |> Stream.map(fn [ds, title] -> %{date: to_date!(ds), title: title} end)  # %{date: date, title: title}
-    |> TodoList.new()
+  defp create_entries(lines) do
+    Stream.map(
+      lines,
+      fn line ->
+        [date_string, title] = String.split(line, ",")
+        date = Date.from_iso8601!(date_string)
+        %{date: date, title: title}
+      end
+    )
   end
 end
