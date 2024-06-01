8377f606d956f548fd1970d0821e4f3ecc9ce96a
Shun Usami <usatie@gmail.com>
2024-06-01 15:03:15 -0700

    [Elixir] Add more tests

--- elixir_in_action/ch07/todo/lib/todo/list.ex
+++ elixir_in_action/ch07/todo/lib/todo/list.ex
@@ -9,6 +9,10 @@ defmodule Todo.List do
     )
   end
 
+  def size(todo_list) do
+    map_size(todo_list.entries)
+  end
+
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.next_id)
     new_entries = Map.put(
