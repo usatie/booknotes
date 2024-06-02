770ebfe009c695e01758ede0b1aa49793818d01f
Shun Usami <usatie@gmail.com>
2024-06-01 17:39:53 -0700

    [Elixir] 7.3.5 Implement pooling

--- elixir_in_action/ch07/todo_cache_pooling/lib/todo/database.ex
+++ elixir_in_action/ch07/todo_cache_pooling/lib/todo/database.ex
@@ -10,39 +10,34 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    worker = choose_worker(key)
+    Todo.DatabaseWorker.store(worker, key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    worker = choose_worker(key)
+    Todo.DatabaseWorker.get(worker, key)
   end
 
   @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    pool = Enum.reduce(0..2, %{}, fn index, workers ->
+      db_folder = Path.join(@db_folder, "#{index}")
+      pid = Todo.DatabaseWorker.start(db_folder)
+      Map.put(workers, index, pid)
+    end)
+    {:ok, pool}
+  end
+
+  def choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
   end
 
   @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, state}
-  end
-
-  @impl GenServer
-  def handle_call({:get, key}, _, state) do
-    data = case File.read(file_name(key)) do
-      {:ok, contents} -> :erlang.binary_to_term(contents)
-      _ -> nil
-    end
-
-    {:reply, data, state}
-  end
-
-  defp file_name(key) do
-    Path.join(@db_folder, to_string(key))
+  def handle_call({:choose_worker, key}, _, pool) do
+    index = :erlang.phash2(key, 3)
+    {:ok, worker} = Map.get(pool, index)
+    {:reply, worker, pool}
   end
 end
